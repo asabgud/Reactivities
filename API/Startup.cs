4f0b49c4be9499305eefd4311c24dca1b6714716
Ása <asabgud@gmail.com>
2021-05-17 16:49:57 +0000

    End of SEction 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,13 @@ namespace API
 
             services.AddControllers();
             services.AddApplicationServices(_config);
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,22 +1,30 @@
-import React, {  useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
-
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent'
 
 function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+      let activities: Activity[] = [];
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   }, []) //[] ensures that this will only run once, I think because activities is not populated, it will happen in the state
 
@@ -38,18 +46,44 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity){
-    activity.id 
-    ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-    : setActivities([...activities, {...activity, id: uuid()}]); //adding to the array
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true); 
+
+    if(activity.id){//creat,ing
+      console.log(activity);
+      agent.Activities.update(activity).then(() => {
+
+        console.log('here 1');
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        console.log('here 2');
+        setSelectedActivity(activity);
+        console.log('here 3');
+        setEditMode(false);
+        setSubmitting(false); 
+        console.log('here 4');
+
+      })
+    } else { //updating
+        activity.id = uuid();
+        agent.Activities.create(activity).then(() => {
+            setActivities([...activities, activity])
+            setSelectedActivity(activity);
+            setEditMode(false);
+            setSubmitting(false);
+        })
+    }
   }
+
 
   function handleDeleteActivity(id: string){
-    setActivities([...activities.filter((x => x.id !== id))])
+    setSubmitting(true);
+    agent.Activities.delete(id).then( () => {
+      setActivities([...activities.filter((x => x.id !== id))])
+      setSubmitting(false);
+    })
+    
   }
 
-
+  if (loading) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
@@ -65,10 +99,13 @@ function App() {
             closeForm = {handleFormClose}
             createOrEdit = {handleCreateOrEditActivity}
             deleteActivity = {handleDeleteActivity}
+            submitting = {submitting}
           />
         </Container>
     </>
   );
 
-  }
+  
+}
+
 export default App;
