66a4a50255f3863ccb6caa972a2c43b8cbe6da0b
Ása <asabgud@gmail.com>
2021-05-17 00:06:36 +0000

    End of Section 5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app/layout/styles.css'
+import './app/layout/App';
 import 'semantic-ui-css/semantic.min.css';
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 
